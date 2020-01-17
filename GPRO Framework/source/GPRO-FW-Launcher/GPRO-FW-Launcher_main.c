7588b8f854e5e00912c48773ac9f481b4b1c344e
madilyn.baer <madilyn.baer@mymail.champlain.edu>
2020-01-17 16:47:24 -0500

    Created lib, project added some code

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
@@ -1,7 +1,17 @@
 // GPRO-FW-Launcher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <stdio.h>
+
+int foo(int bar)
+{
+	return (bar + 1);
+}
+
+
 int main()
 {
+	int test = foo(9000);
 
+	printf("\n%d", test);
 }
